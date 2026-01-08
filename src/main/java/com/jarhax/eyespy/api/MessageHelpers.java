7f5f95003cf9f177e9cc1ac7d5b52935e7438480
Tyler Hancock <darklime@live.ca>
2026-01-07 18:19:24 -0700

    Add some localizations based on old mods.

---
+++ src/main/java/com/jarhax/eyespy/api/MessageHelpers.java
@@ -0,0 +1,34 @@
+package com.jarhax.eyespy.api;
+
+import com.hypixel.hytale.server.core.Message;
+
+public class MessageHelpers {
+
+    public static Message tier(int tier) {
+        return Message.translation("server.eyespy.tier").param("tier", tier);
+    }
+
+    public static Message level(int tier) {
+        return Message.translation("server.eyespy.level").param("level", tier);
+    }
+
+    public static Message capacity(int capacity) {
+        return Message.translation("server.eyespy.capacity").param("capacity", capacity);
+    }
+
+    public static Message size(int size) {
+        return Message.translation("server.eyespy.size").param("size", size);
+    }
+
+    public static Message progress(int current, int max) {
+        return progress((float) current / max);
+    }
+
+    public static Message progress(float amount) {
+        return Message.translation("server.eyespy.progress").param("progress", amount);
+    }
+
+    public static Message stage(int current, int max) {
+        return Message.translation("server.eyespy.stage").param("current", current).param("max", max);
+    }
+}
